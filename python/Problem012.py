45984c82a951d933c81caeb3bb7ee575e5a46d37
Will Metcher <will.metcher@gmail.com>
2019-06-04 22:39:31 +1000

    Problem 12

--- python/Problem012.py
+++ python/Problem012.py
@@ -1,16 +1,14 @@
+#76576500 in 11.66162109375 seconds
 import time
 
 from EulerUtils import *
 
 start = time.time()
 count = 0
-
-for i in range(1, 99999999):
+for i in range(1, 99999999, 1):
     count += i
-    if is_prime(count) != None:
-        factors = get_factors(count)
-        if len(factors) > 500:
+    if is_prime(count) == False:
+        if len(get_factors(count)) > 500:
             end = time.time()
             print(count,"in",end-start,"seconds")
             break
-    i += 1
